a67cc4fd5587dbd7622d7cdd5f7ecb6cab6ba23a
Nícolas Iensen <nicolas@quavio.com.br>
2011-10-11 10:02:50 -0300

    Bug fix

--- app/controllers/insurances_controller.rb
+++ app/controllers/insurances_controller.rb
@@ -1,6 +1,12 @@
 class InsurancesController < InheritedResources::Base
   before_filter :only => [:index] { @insurance = Insurance.new; @client = Client.new }
 
+  def create
+    create! do |success, failure|
+      success.html { redirect_to insurances_path, :notice => "Feito! Cliente inserido" }
+    end
+  end
+
   def update
     update! do |success, failure|
       success.html { redirect_to insurances_path, :notice => "Woohoo! Seguro atualizado" }
