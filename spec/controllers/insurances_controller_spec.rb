5850318e33dc4ea1133f6e2b5440d7f1c1bac848
Nícolas Iensen <nicolas.iensen@gmail.com>
2021-01-03 14:52:35 +0100

    Replace rspec's stub! with stub

--- spec/controllers/insurances_controller_spec.rb
+++ spec/controllers/insurances_controller_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe InsurancesController do
   describe "GET index" do
-    before { controller.stub!(:current_user).and_return(User.new(nickname: "nicolas.iensen@gmail.com")) }
+    before { controller.stub(:current_user).and_return(User.new(nickname: "nicolas.iensen@gmail.com")) }
     before { get :index }
     it { should assign_to(:insurance) }
   end
 
   describe "PUT update" do
     let(:insurance){ insurance = stub_model(Insurance, :start_at => Date.today, :value => 1000, :commission => 20) }
-    before { controller.stub!(:current_user).and_return(User.new(nickname: "nicolas.iensen@gmail.com")) }
+    before { controller.stub(:current_user).and_return(User.new(nickname: "nicolas.iensen@gmail.com")) }
     before { Insurance.stub(:find).with("1").and_return(insurance) }
     before { put :update, :id => 1 }
     it { should redirect_to(insurances_path) }
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe SessionsController do
   describe "GET new" do
     context "when it's already logged in" do
-      before { controller.stub!(:signed_in?).and_return(true) }
+      before { controller.stub(:signed_in?).and_return(true) }
       before { get :new }
       it { should redirect_to root_path }
     end
