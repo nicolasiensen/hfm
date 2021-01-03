9cdcbc12bdcd91d932d9d486cd2322014c4bec0d
Nícolas Iensen <nicolas.iensen@gmail.com>
2021-01-03 14:51:21 +0100

    Fix rspec warnings

--- spec/controllers/clients_controller_spec.rb
+++ spec/controllers/clients_controller_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe ClientsController do
   describe "GET index" do
-    before { controller.stub!(:current_user).and_return(mock_model(User, :nickname => "nicolas.iensen@gmail.com")) }
+    before { controller.stub!(:current_user).and_return(User.new(nickname: "nicolas.iensen@gmail.com")) }
     before { get :index }
     it { should assign_to(:client) }
   end
 
   describe "POST create" do
-    before { controller.stub!(:current_user).and_return(mock_model(User, :nickname => "nicolas.iensen@gmail.com")) }
+    before { controller.stub!(:current_user).and_return(User.new(nickname: "nicolas.iensen@gmail.com")) }
     before { Client.stub(:create).and_return true }
     before { post :create, :client => {} }
     it { should redirect_to(clients_path) }
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -5,21 +5,23 @@ describe Company do
 
   describe "#income" do
     context "when there is no insurance" do
-      its(:income){should be_zero}
+      it { subject.income.should be_zero }
     end
+
     context "when there is some insurances" do
-      before { subject.stub(:insurances).and_return([mock_model(Insurance, :income => 1000), mock_model(Insurance, :income => 2000)]) }
-      its(:income){should be_eql 3000}
+      before { subject.stub(:insurances).and_return([Insurance.new(income: 1000), Insurance.new(income: 2000)]) }
+      it { subject.income.should be_eql(3000) }
     end
   end
 
   describe "#value" do
     context "when there is no insurance" do
-      its(:value){should be_zero}
+      it { subject.value.should be_zero }
     end
+
     context "when there is some insurances" do
-      before { subject.stub(:insurances).and_return([mock_model(Insurance, :value => 1000), mock_model(Insurance, :value => 2000)]) }
-      its(:value){should be_eql 3000}
+      before { subject.stub(:insurances).and_return([Insurance.new(value: 1000), Insurance.new(value: 2000)]) }
+      it { subject.value.should be_eql(3000) }
     end
   end
 
